a8401435e32f4d58604e9874293ea1d241b45abe
duong\DUONG <duomg0023@gmail.com>
2024-10-07 23:33:21 +0700

    connectDB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,22 @@
+import db from "../models/index.js";
+const getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log("-----------------")
+        console.log(data)
+        return res.render("homepage.ejs", {
+            data: JSON.stringify(data)
+        });
+    } catch (error) {
+        console.log("Error", error);
 
-const getHomePage = (req, res) => {
-    return res.render("homepage.ejs");
+    }
+
 }
 const getAboutPage = (req, res) => {
     return res.render("test/about.ejs");
 }
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage:getAboutPage,
+    getAboutPage: getAboutPage,
 }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import initWebRoutes from "./route/web";
 import bodyParser from "body-parser";
 import configViewEngine from "./config/viewEngine";
+import connectDB from "./config/connectDB";
 require('dotenv').config();
 
 
@@ -15,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 configViewEngine(app);
 initWebRoutes(app);
 
+// connect to database
+connectDB();
 const port = process.env.PORT || 4444;
 app.listen(port, () => {
     console.log("Backend Nodejs is running on port:" + port);
